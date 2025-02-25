c86da3a04666e84d3fc6c5ba4b8a1667d9d0f3c4
Niloxp753 <niloxp.753@gmail.com>
2025-02-25 20:30:46 -0300

    chore: :memo: `Update ecosystem.config.js and package.json for production deployment`

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,35 @@
 module.exports = {
     apps: [
-        {
-            name: "proxy",
-            script: "./server.js",
-            env: {
-              PORT: 3000, // Porta que o Render vai expor
-            },
+      {
+        name: "proxy",
+        script: "./server.js",
+        env: {
+          PORT: 3000, // Porta que o Render vai expor
         },
-        {
-            name: "lush_ipiranga",
-            script: "./lush_ipiranga/dist/main.js",  // Caminho correto para o arquivo main.js
-            env: {
-                PORT: 3001,
-                NODE_ENV: "production",
-                DATABASE_URL_LOCAL_IPIRANGA: process.env.DATABASE_URL_LOCAL_IPIRANGA,
-                DATABASE_URL_ONLINE_IPIRANGA: process.env.DATABASE_URL_ONLINE_IPIRANGA
-            }
+      },
+      {
+        name: "lush_ipiranga",
+        script: "./lush_ipiranga/dist/main.js",  // Caminho correto para o arquivo main.js
+        env: {
+          PORT: 3001,
+          NODE_ENV: "production",
+          DATABASE_URL_LOCAL_IPIRANGA: process.env.DATABASE_URL_LOCAL_IPIRANGA,
+          DATABASE_URL_ONLINE_IPIRANGA: process.env.DATABASE_URL_ONLINE_IPIRANGA,
         },
-        {
-            name: "lush_lapa",
-            script: "./lush_lapa/dist/main.js",  // Caminho correto para o arquivo main.js
-            env: {
-                PORT: 3002,
-                NODE_ENV: "production",
-                DATABASE_URL_LOCAL_LAPA: process.env.DATABASE_URL_LOCAL_LAPA,
-                DATABASE_URL_ONLINE_LAPA: process.env.DATABASE_URL_ONLINE_LAPA
-            }
-        }
+      },
+      {
+        name: "lush_lapa",
+        script: "./lush_lapa/dist/main.js",  // Caminho correto para o arquivo main.js
+        env: {
+          PORT: 3002,
+          NODE_ENV: "production",
+          DATABASE_URL_LOCAL_LAPA: process.env.DATABASE_URL_LOCAL_LAPA,
+          DATABASE_URL_ONLINE_LAPA: process.env.DATABASE_URL_ONLINE_LAPA,
+        },
+      },
+    ],
+    post_deploy: [
+      "npm install -g pm2"  // Instalar o PM2 globalmente após o deploy
     ]
-};
+  };
+  
