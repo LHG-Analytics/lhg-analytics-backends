0260ff2cda2dfe58734c4988298e1b3ef13bf6d4
Niloxp753 <niloxp.753@gmail.com>
2025-02-25 20:47:27 -0300

    chore: :memo: `Update exec command to use npx in server.js`

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("pm2 start ecosystem.config.js", (err, stdout, stderr) => {
+exec("npx pm2 start ecosystem.config.js", (err, stdout, stderr) => {
   if (err) {
     console.error(`Exec error: ${err}`);
     return;
