13798f062f964f8a8a98bdf33b206a9108f8b3a1
Niloxp753 <niloxp.753@gmail.com>
2025-02-26 04:19:58 -0300

    chore: :memo: `Update server.js to use different proxy targets and ports for Lush Ipiranga and Lush Lapa`

--- server.js
+++ server.js
@@ -5,21 +5,22 @@ const app = express();
 
 // Proxy para o backend do Lush Ipiranga
 app.use('/lush_ipiranga', createProxyMiddleware({
-  target: 'https://lhg-analytics-backend-bmmd.onrender.com/lush_ipiranga',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/lush_ipiranga': '',
-  },
-}));
-
-// Proxy para o backend do Lush Lapa
-app.use('/lush_lapa', createProxyMiddleware({
-  target: 'https://lhg-analytics-backend-bmmd.onrender.com/lush_lapa',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/lush_lapa': '',
-  },
-}));
+    target: 'https://lhg-analytics-backend-bmmd.onrender.com:3001',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/lush_ipiranga': '', // Remove o prefixo para que o caminho interno seja correto
+    },
+  }));
+  
+  // Proxy para o backend do Lush Lapa
+  app.use('/lush_lapa', createProxyMiddleware({
+    target: 'https://lhg-analytics-backend-bmmd.onrender.com:3002',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/lush_lapa': '', // Remove o prefixo para que o caminho interno seja correto
+    },
+  }));
+  
 
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
