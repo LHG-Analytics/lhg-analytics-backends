63cf7a88e3cab3ead7f2ffd3472eda6fbaae7a68
Niloxp753 <niloxp.753@gmail.com>
2025-02-25 21:32:41 -0300

    chore: :fire: `Removed PM2 startup command and updated proxy configuration`

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const { exec } = require("child_process");
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
-
-// Iniciar PM2 para rodar os serviços
-exec("npx pm2 start ecosystem.config.js", (err, stdout, stderr) => {
-  if (err) {
-    console.error(`Erro ao iniciar PM2: ${err}`);
-    return;
-  }
-  console.log(`PM2 iniciado:\n${stdout}`);
-  console.error(`PM2 stderr:\n${stderr}`);
-});
 
 // Configurar proxy reverso para os serviços
 const targets = {
