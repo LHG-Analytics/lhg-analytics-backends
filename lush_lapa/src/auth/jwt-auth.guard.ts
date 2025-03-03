7b1907b31892130889ccf8919dbace4b06af1fb6
Niloxp753 <niloxp.753@gmail.com>
2025-03-03 17:47:18 -0300

    refactor: :memo: `Removed PrismaService and async canActivate method, updated token extraction and verification logic`

--- lush_lapa/src/auth/jwt-auth.guard.ts
+++ lush_lapa/src/auth/jwt-auth.guard.ts
@@ -5,13 +5,10 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
-import { PrismaService } from '../prisma/prisma.service'; // Importa o PrismaService
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(private readonly prisma: PrismaService) {} // Injeta o PrismaService
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
     const authHeader = request.headers.authorization;
 
@@ -23,30 +20,16 @@ export class JwtAuthGuard implements CanActivate {
       );
     }
 
-    const token = authHeader.split(' ')[1];
+    const token = authHeader.split(' ')[1]; // Extrai o token do header
 
     if (!process.env.NEXTAUTH_SECRET) {
       throw new Error('NEXTAUTH_SECRET não definido no ambiente');
     }
 
     try {
-      const decoded = jwt.verify(token, process.env.NEXTAUTH_SECRET) as {
-        email: string;
-        id: string;
-      };
-
+      const decoded = jwt.verify(token, process.env.NEXTAUTH_SECRET);
+      request.user = decoded; // Adiciona os dados do usuário na requisição
       console.log('Usuário autenticado:', decoded);
-
-      // 🔹 Verifica se o usuário existe no banco de dados
-      const user = await this.prisma.prismaOnline.user.findUnique({
-        where: { email: decoded.email },
-      });
-
-      if (!user) {
-        throw new UnauthorizedException('Usuário não encontrado no banco');
-      }
-
-      request.user = user; // Armazena o usuário na requisição
       return true;
     } catch (error) {
       console.error('Erro ao validar token:', error.message);
