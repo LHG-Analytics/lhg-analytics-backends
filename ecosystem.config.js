ddcfa69f9f4b14f0868953ff4ef9d033d1123fc8
Niloxp753 <niloxp.753@gmail.com>
2025-03-04 11:43:53 -0300

    refactor: :memo: `Update ecosystem.config.js to use ES module syntax and remove unused configurations`

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,47 +1,43 @@
 module.exports = {
-    apps: [
-      {
-        name: "proxy",
-        script: "./server.js",
-        env: {
-          PORT: 3000, // Proxy sempre na porta 3000
-        },
-        env_production: {
-          PORT: 3000, // Proxy sempre na porta 3000
-        },
+  apps: [
+    {
+      name: "proxy",
+      script: "./server.mjs",
+      env: {
+        PORT: 3000, // Proxy sempre na porta 3000
       },
-      {
-        name: "lush_ipiranga",
-        script: "./lush_ipiranga/dist/main.js",
-        env: {
-          NODE_ENV: "production",
-          DATABASE_URL_LOCAL_IPIRANGA: process.env.DATABASE_URL_LOCAL_IPIRANGA,
-          DATABASE_URL_ONLINE_IPIRANGA: process.env.DATABASE_URL_ONLINE_IPIRANGA,
-          SERVICE_PREFIX: "ipiranga",
-          PORT: 3001, // Rodando localmente
-        },
-        env_production: {
-          PORT: 3001, // Rodando localmente
-        },
+      env_production: {
+        PORT: 3000, // Proxy sempre na porta 3000
       },
-      {
-        name: "lush_lapa",
-        script: "./lush_lapa/dist/main.js",
-        env: {
-          NODE_ENV: "production",
-          DATABASE_URL_LOCAL_LAPA: process.env.DATABASE_URL_LOCAL_LAPA,
-          DATABASE_URL_ONLINE_LAPA: process.env.DATABASE_URL_ONLINE_LAPA,
-          SERVICE_PREFIX: "lapa",
-          PORT: 3002, // Rodando localmente
-        },
-        env_production: {
-          PORT: 3002, // Rodando localmente
-        },
+    },
+    {
+      name: "lush_ipiranga",
+      script: "./lush_ipiranga/dist/main.js",
+      env: {
+        NODE_ENV: "production",
+        DATABASE_URL_LOCAL_IPIRANGA: process.env.DATABASE_URL_LOCAL_IPIRANGA,
+        DATABASE_URL_ONLINE_IPIRANGA: process.env.DATABASE_URL_ONLINE_IPIRANGA,
+        SERVICE_PREFIX: "ipiranga",
+        PORT: 3001, // Rodando localmente
       },
-    ],
-    post_deploy: [
-      "npm install --legacy-peer-deps",
-      "npm install -g pm2",
-    ],
-  };
-  
+      env_production: {
+        PORT: 3001, // Rodando localmente
+      },
+    },
+    {
+      name: "lush_lapa",
+      script: "./lush_lapa/dist/main.js",
+      env: {
+        NODE_ENV: "production",
+        DATABASE_URL_LOCAL_LAPA: process.env.DATABASE_URL_LOCAL_LAPA,
+        DATABASE_URL_ONLINE_LAPA: process.env.DATABASE_URL_ONLINE_LAPA,
+        SERVICE_PREFIX: "lapa",
+        PORT: 3002, // Rodando localmente
+      },
+      env_production: {
+        PORT: 3002, // Rodando localmente
+      },
+    },
+  ],
+  post_deploy: ["npm install --legacy-peer-deps", "npm install -g pm2"],
+};
