798f6673987138d554d1a57851f1474772706786
Niloxp753 <niloxp.753@gmail.com>
2025-03-04 12:00:27 -0300

    refactor: :memo:

--- server.mjs
+++ server.mjs
@@ -3,13 +3,16 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 
-const proxyConfig = (target) =>
+const proxyConfig = (target, pathRewrite) =>
   createProxyMiddleware({
     target,
     changeOrigin: true,
-    preserveHeaderKeyCase: true,
+    pathRewrite,
+    logLevel: "debug", // <-- Adiciona logs detalhados no terminal
     onProxyReq: (proxyReq, req, res) => {
-      console.log(`🔄 Proxy encaminhando requisição para: ${target}`);
+      console.log(
+        `🔄 Proxy encaminhando requisição para: ${target}${req.originalUrl}`
+      );
       console.log("Headers recebidos no Proxy:", req.headers);
       if (req.headers["authorization"]) {
         proxyReq.setHeader("Authorization", req.headers["authorization"]);
@@ -19,15 +22,15 @@ const proxyConfig = (target) =>
 
 app.use(
   "/lush_ipiranga",
-  proxyConfig(
-    "https://lhg-analytics-backend-bmmd.onrender.com/lush_ipiranga/ipiranga/api"
-  )
+  proxyConfig("https://lhg-analytics-backend-bmmd.onrender.com", {
+    "^/lush_ipiranga": "",
+  })
 );
 app.use(
   "/lush_lapa",
-  proxyConfig(
-    "https://lhg-analytics-backend-bmmd.onrender.com/lush_lapa/lapa/api"
-  )
+  proxyConfig("https://lhg-analytics-backend-bmmd.onrender.com", {
+    "^/lush_lapa": "",
+  })
 );
 
 const port = process.env.PORT || 3000;
