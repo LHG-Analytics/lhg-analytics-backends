4474df3b6e325172df957772ed02a0619eb0edca
Niloxp753 <niloxp.753@gmail.com>
2025-04-29 22:05:25 -0300

    refactor: :memo: `Update proxy configuration for "/auth/api" to "/api" in server.mjs`

--- server.mjs
+++ server.mjs
@@ -20,7 +20,7 @@ const proxyConfig = (target) =>
     },
   });
 
-app.use("/auth/api", proxyConfig("http://localhost:3005"));
+app.use("/api", proxyConfig("http://localhost:3005"));
 app.use("/lush_ipiranga", proxyConfig("http://localhost:3001"));
 app.use("/lush_lapa", proxyConfig("http://localhost:3002"));
 app.use("/tout", proxyConfig("http://localhost:3003"));
